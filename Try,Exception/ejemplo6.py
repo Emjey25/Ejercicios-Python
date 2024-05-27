1ad4db163b8054abcc310aa007650218da0b1eb7
MaryJoseph/ <majocausil16@gmail.com>
2024-05-27 04:15:46 -0500

    Ejercicios clase7 Ucoltis , Try, exception,else,finally

---
+++ Try,Exception/ejemplo6.py
@@ -0,0 +1,21 @@
+dic = {1: 'a', 2: 'b', 3: 'c'}
+
+#KeyError: se produce cuando se utiliza una llave que no existe en el diccionario
+
+#FileNotFundError: se produce cuando se intenta abrir un archivo que no existe
+
+#ImportError: se produce cuando se intenta importar un modulo que no existe
+    
+lista_grande = [0]* 10**8
+print(lista_grande)
+
+#MemoryError: se produce cuando se intenta reservar memoria que no existe
+
+#OverflowError: se produce cuando se intenta almacenar un numero muy grande
+
+#ZeroDivisionError: se produce cuando se intenta dividir por cero
+
+#TypeError: se produce cuando se intenta operar con tipos de datos incompatibles
+
+#ValueError: se produce cuando se intenta utilizar un valor incorrecto
+
